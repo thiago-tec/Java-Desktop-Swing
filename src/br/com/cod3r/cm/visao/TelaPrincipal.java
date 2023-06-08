891db8f185fd9ad2afd5d543c4182a35dcfac361
Thiago de jesus Cordeiro <thiago.cordeiros@hotmail.com>
2023-06-08 04:06:55 -0300

    commit Java Desktop com swing

---
+++ src/br/com/cod3r/cm/visao/TelaPrincipal.java
@@ -0,0 +1,30 @@
+package br.com.cod3r.cm.visao;
+
+import javax.swing.JFrame;
+
+import br.com.cod3r.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class TelaPrincipal extends JFrame{
+
+	public TelaPrincipal() {
+		Tabuleiro tabuleiro = new Tabuleiro(16,30,50);
+		add( new PainelTabuleiro(tabuleiro));
+		
+		setTitle("campo minado");
+		setSize(690, 438);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		
+		new TelaPrincipal();
+		
+		
+		
+	}
+	
+	
+}
